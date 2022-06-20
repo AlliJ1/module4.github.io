7078b90c5040a2ba5e2d184e1cebc1668c678d02
AlliJ1 <104732946+AlliJ1@users.noreply.github.com>
2022-06-20 01:59:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 (function () { 
-    var names = ["Alli", "Alisha", "June", "jaquan", "jeniza", "jellen", "Jesus", "June", "Julia", "Juli"];
-/*	var names =new Array();
-names[0]=" Alli";
-names[1]=" Alisha";
-names[2]=" June";
-names[3]=" jaquan";
-names[4]=" jeniza";
-names[5]=" jellen";
-names[6]=" Julia";
-names[7]=" Juli";
-names[9]=" Jesus";
-*/
+   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
 
 	if (names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
